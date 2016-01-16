ec258d31df70e4fa47bfbca33c16fde545c77177
Dominic Taylor <dataylorx@gmail.com>
2016-01-16 17:30:34 +1300

    Add listener to racer.js

--- js/racer.js
+++ js/racer.js
@@ -1,1 +1,13 @@
-a function that each time you press a key moves the active state across one table cell. 
+/*a function that each time you press a key moves the active state across one table cell. 
+they both start in the firs cell. then use #active to move the black dot. 
+p keyCode 80
+q keyCode 81*/
+
+
+document.addEventListener('keydown', updatePlayer(), false);
+
+
+function updatePlayer () {     
+        console.log("E");
+    }
+  
